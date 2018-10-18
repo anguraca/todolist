e3ff6547b40734f7e4cc94f8b6b65b7ee278cf6e
Fredy_Vanegas <fredy.vanegas@unincca.edu.co>
2018-10-18 16:24:44 -0500

    proyecto

--- app.js
+++ app.js
@@ -17,7 +17,29 @@ app.use(session({
     secret: 'node'
 })); //Configuración cookie-session (persistencia en sesión)
 
+app.set('view engine', 'ejs'); //Configuración de template engine EJS
 
+app.use('/public',express.static('public'));
+let tareas = ['uno','dos'];
+
+app.get('/', function (request, response) {
+    response.render('formulario.ejs', {
+tareas
+    });
+});
+
+app.post('/adicionar',function(request,response){
+    let tarea = request.body.nuevaTarea;
+    tareas.push(tarea);
+    response.redirect('/');
+})
+app.get('/borrar/:id',function(request,response){
+    //let tarea = request.body.nuevaTarea;
+    let id = +request.params.id;
+    console.log(params);
+    tareas.splice(id, 1);
+    response.redirect('/');
+})
 //verificar en qué puerto se está trabajando
 app.listen(port, function () {
     console.log('Escuchando en el puerto ', port);
